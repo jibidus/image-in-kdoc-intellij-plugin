b9c116927dffa6bc2d9bf99f6b22c0a525452016
Jibidus <jibidus@gmail.com>
2025-04-07 07:26:01 +0200

    Extract getData() from Sampl in CustomRenderer

--- src/main/kotlin/org/jibidus/intellij/customdebuggerrenderer/CustomRenderer.kt
+++ src/main/kotlin/org/jibidus/intellij/customdebuggerrenderer/CustomRenderer.kt
@@ -6,20 +6,23 @@ import com.intellij.debugger.impl.DebuggerUtilsImpl
 import com.intellij.debugger.ui.impl.watch.ValueDescriptorImpl
 import com.intellij.debugger.ui.tree.render.CompoundRendererProvider
 import com.intellij.debugger.ui.tree.render.CustomPopupFullValueEvaluator
+import com.intellij.diff.fragments.LineFragmentImpl
 import com.intellij.openapi.diagnostic.Logger
+import com.intellij.openapi.diagnostic.LoggerRt
 import com.intellij.rt.debugger.ImageSerializer
+import com.sun.jdi.ObjectReference
 import com.sun.jdi.StringReference
 import com.sun.jdi.Value
 import org.intellij.images.editor.impl.ImageEditorManagerImpl.createImageEditorUI
 import org.jetbrains.annotations.Nls
 import java.awt.GraphicsEnvironment.getLocalGraphicsEnvironment
-import java.awt.Stroke
 import java.awt.Transparency.TRANSLUCENT
-import java.awt.image.BufferedImage
 import java.nio.charset.StandardCharsets
 import javax.swing.Icon
 import javax.swing.ImageIcon
 import javax.swing.JComponent
+import javax.swing.JLabel
+import javax.swing.JPanel
 
 // Cf ImageObjectRenderer
 class CustomRenderer : CompoundRendererProvider() {
@@ -40,22 +43,41 @@ class CustomRenderer : CompoundRendererProvider() {
 
     override fun getFullValueEvaluatorProvider(): FullValueEvaluatorProvider {
         return FullValueEvaluatorProvider { evaluationContext: EvaluationContextImpl, valueDescriptor: ValueDescriptorImpl ->
-            object: CustomPopupFullValueEvaluator<String>(
+            object : CustomPopupFullValueEvaluator<String>(
                 "\\u2026 Show Sample",
                 evaluationContext
             ) {
-                override fun getData(): String {
-                    return "bla"
+                override fun getData(): String? {
+                    val obj = valueDescriptor.value as? ObjectReference ?: return null
+                    LOG.info("##### obj.javaClass=${obj.javaClass}")
+                    LOG.info("##### obj.referenceType()=${obj.referenceType()}")
+//                    for (method in obj.referenceType().methods()) {
+//                        LOG.info("#### method name '${method.name()}' with signature '${method.signature()}'")
+//                    }
+                    val data = DebuggerUtilsImpl.invokeObjectMethod(
+                        getEvaluationContext().createEvaluationContext(obj),
+                        obj,
+                        "getData",
+                        "()Ljava/lang/String;",
+                        emptyList()
+                    ) as StringReference
+                    LOG.info("##### data=${data}")
+                    return data.value()
                 }
 
                 override fun createComponent(data: String): JComponent {
-                    val image = getLocalGraphicsEnvironment()
-                        .defaultScreenDevice.defaultConfiguration
-                        .createCompatibleImage(200, 200, TRANSLUCENT)
-                    val g = image.createGraphics()
-                    g.drawRect(30,30,100,100)
-                    g.dispose()
-                    return createImageEditorUI(image)
+//                    val image = getLocalGraphicsEnvironment()
+//                        .defaultScreenDevice.defaultConfiguration
+//                        .createCompatibleImage(200, 200, TRANSLUCENT)
+//                    val g = image.createGraphics()
+//                    g.drawRect(30, 30, 100, 100)
+//                    g.dispose()
+//                    return createImageEditorUI(image)
+
+                    return JPanel().apply {
+                        add(JLabel(data))
+                    }
+
                 }
             }
 
